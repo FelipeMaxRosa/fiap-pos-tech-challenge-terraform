3aba880281b48f289847734a626917d90e97c6f1
Felipe Rosa <felipemax.suporteti@gmail.com>
2023-10-15 14:49:38 +0100

    chore: provider improvements

--- 0-provider.tf
+++ 0-provider.tf
@@ -12,11 +12,19 @@ variable "cluster_version" {
 }
 
 terraform {
+  required_version = ">=1.0"
   required_providers {
     aws = {
       source = "hashicorp/aws"
       version = "~> 4.0"
     }
+  }
+
+  backend "s3" {
+    bucket  = "fnf-terraform-fmr"
+    key     = "fnf-terraform-fmr.tfstate"
+    region  = "us-east-1"
+    profile = "terraform"
   }
 }
 
